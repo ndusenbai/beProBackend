7926337e4dc3b4538b02af752941f50e0fa96920
Bakhtiyar Ospanov <b.p.ospanov@gmail.com>
2022-07-11 11:13:01 +0600

    fix: create super user

--- auth_user/models.py
+++ auth_user/models.py
@@ -34,7 +34,11 @@ class UserManager(BaseUserManager):
             raise ValueError('Superuser must have is_staff=True.')
         if extra_fields.get('is_superuser') is not True:
             raise ValueError('Superuser must have is_superuser=True.')
-        return self.create_user(email, password, **extra_fields)
+        email = email.lower()
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
 
 
 class AssistantTypes(models.IntegerChoices):
