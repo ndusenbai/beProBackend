498bf50b92b24c6511505bc4d3ed32df8d789dc0
Bakhtiyar Ospanov <b.p.ospanov@gmail.com>
2022-07-14 15:34:45 +0600

    fix: refactor

--- applications/urls.py
+++ applications/urls.py
@@ -7,7 +7,7 @@ router = DefaultRouter()
 
 router.register('test-application', views.TestApplicationView, basename='test-application-view')
 router.register('tariff-application', views.TariffApplicationView, basename='tariff-application-view')
-router.register('to-create-company', views.ApplicationToCreateCompanyViewSet, basename='to-create-company')
+router.register('app-to-create-company', views.ApplicationToCreateCompanyViewSet, basename='app-to-create-company')
 
 urlpatterns = [
     path('approve-tariff-app/', views.ApproveTariffApplication.as_view(), name='approve-tariff-app')
