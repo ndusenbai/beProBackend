dee5657269eb30bf8897b58c45d075b7647bfce9
Bakhtiyar Ospanov <b.p.ospanov@gmail.com>
2022-07-14 15:59:19 +0600

    fix: role id

--- auth_user/services.py
+++ auth_user/services.py
@@ -202,4 +202,8 @@ def get_additional_user_info(email: str) -> dict:
     user = User.objects.get(email=email)
     user_data = UserModelSerializer(user).data
     user_data['role'] = get_user_role(user)
+    try:
+        user_data['role_id'] = user.role.id
+    except:
+        user_data['role_id'] = None
     return user_data
