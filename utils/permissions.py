dc87026479530035016481d20a16d1190dc26540
Bakhtiyar Ospanov <b.p.ospanov@gmail.com>
2022-07-28 15:59:19 +0600

    fix: department perm

--- utils/permissions.py
+++ utils/permissions.py
@@ -116,6 +116,10 @@ class DepartamentPermissions(BasePermission):
                 if not request.GET.get('company'):
                     return False
 
+            if view.action == 'retrieve':
+                if int(view.kwargs['pk']) == request.user.role.department_id:
+                    return True
+
             if request.user.is_authenticated and (role in {'owner', 'superuser', 'hr', 'observer'}):
                 return True
 
