ddb7e5b566e7cb6e5908c9af6c2c2dbf91b28a67
Bakhtiyar Ospanov <b.p.ospanov@gmail.com>
2022-07-29 18:14:08 +0600

    fix: admin

--- timesheet/admin.py
+++ timesheet/admin.py
@@ -19,6 +19,6 @@ class EmployeeScheduleAdmin(admin.ModelAdmin):
 @admin.register(TimeSheet)
 class TimeSheetAdmin(admin.ModelAdmin):
     list_display = ('id', 'role', 'day', 'status', 'time_from', 'time_to', 'check_in', 'check_out', 'comment')
-    search_fields = ('role',)
+    search_fields = ('role__user__email',)
     raw_id_fields = ('role',)
     date_hierarchy = 'day'
