af2fa8afbafbe0c1769ba4695824397d8e550763
azamat7087 <azaza7087@gmail.com>
2022-08-01 12:56:45 +0600

    Updated handling checkin exception

--- bepro_statistics/services.py
+++ bepro_statistics/services.py
@@ -42,11 +42,17 @@ def create_statistic(serializer):
 @atomic
 def create_user_statistic(role: Role, data: OrderedDict):
     last_check_in = TimeSheet.objects.filter(role=role, check_out__isnull=True).order_by('-day').first()
+
+    if not last_check_in or last_check_in.day != datetime.now().date():
+        return {'message': 'Вы не осуществляли check in сегодня', }, 400
+
     UserStatistic.objects.create(
         role=role,
         statistic_id=data['statistic_id'],
         day=last_check_in.day,
         fact=data['fact'])
+
+    return {'message': 'created', }, 200
 
 
 @atomic
