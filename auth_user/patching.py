ec5dd4377e834ac6ecbc1c3a5c973c0f3aea9c42
Bakhtiyar Ospanov <b.p.ospanov@gmail.com>
2022-08-10 16:00:23 +0600

    fix: PatchedAPIView

--- auth_user/patching.py
+++ auth_user/patching.py
@@ -19,16 +19,16 @@ class PatchedAPIView(OriginalAPIView):
         role = get_user_role(request.user)
 
         if role in ['superuser', 'admin_marketing', 'admin_production_worker']:
-            return OriginalAPIView.check_permissions(self, request)
+            return super().check_permissions(self, request)
         for url in allowed_urls:
             if request.path.startswith(url):
-                return OriginalAPIView.check_permissions(self, request)
+                return super().check_permissions(self, request)
 
         if not request.user.id:
             return JsonResponse({'message': 'Залогиньтесь'}, status=status.HTTP_403_FORBIDDEN)
 
         if request.user.selected_company.is_active:
-            return OriginalAPIView.check_permissions(self, request)
+            return super().check_permissions(self, request)
 
         return JsonResponse({'message': 'Тариф закончился. Ваша компания не активна'}, status=status.HTTP_403_FORBIDDEN)
 
