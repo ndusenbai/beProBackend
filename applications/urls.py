11ee48600680c9d0f207e9beeb0cf456d941b30d
Bakhtiyar Ospanov <b.p.ospanov@gmail.com>
2022-08-10 12:04:54 +0600

    fix: global check if selected_company is active. refuse if not active

--- applications/urls.py
+++ applications/urls.py
@@ -1,4 +1,3 @@
-from django.urls import path
 from rest_framework.routers import DefaultRouter
 
 from applications import views
@@ -8,5 +7,6 @@ router = DefaultRouter()
 router.register('test-application', views.TestApplicationView, basename='test-application-view')
 router.register('tariff-application', views.TariffApplicationView, basename='tariff-application-view')
 router.register('app-to-create-company', views.ApplicationToCreateCompanyViewSet, basename='app-to-create-company')
+router.register('app-to-create-company/create', views.ApplicationToCreateCompanyCreateViewSet, basename='app-to-create-company-create')
 
 urlpatterns = [] + router.urls
