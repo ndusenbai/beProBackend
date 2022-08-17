2e9855026c5d4a6f4f241b3e83405b53e8ce2063
Bakhtiyar Ospanov <b.p.ospanov@gmail.com>
2022-08-17 12:02:23 +0600

    feat: test-one

--- tests/services/test_one.py
+++ tests/services/test_one.py
@@ -1,4 +1,3 @@
-from typing import OrderedDict
 
 
 class TestOne:
@@ -1230,7 +1229,7 @@ class TestOne:
             '103': 6,
             '102': 0,
             '101': -2,
-            '100':- 4,
+            '100': - 4,
             '99': -6,
             '98': -8,
             '97': -10,
@@ -1379,204 +1378,29 @@ class TestOne:
             '54': -100,
         }
 
-
-class TestTwoAnswers:
-    A = {
-        '1': 'c',
-        '2': 'c',
-        '3': 'd',
-        '4': 'd',
-        '5': 'c',
-        '6': 'e',
-        '7': 'c',
-        '8': 'a',
-        '9': 'd',
-        '10': 'c',
-
-        '11': 'e',
-        '12': 'e',
-        '13': 'b',
-        '14': 'd',
-        '15': 'd',
-        '16': 'c',
-        '17': 'b',
-        '18': 'a',
-        '19': 'd',
-        '20': 'e',
-
-        '21': 'd',
-        '22': 'e',
-        '23': 'c',
-        '24': 'c',
-        '25': 'c',
-        '26': 'd',
-        '27': 'b',
-        '28': 'e',
-        '29': 'e',
-        '30': 'c',
-
-        '31': 'a',
-        '32': 'd',
-        '33': 'b',
-        '34': 'c',
-        '35': 'c',
-        '36': 'c',
-        '37': 'b',
-        '38': 'c',
-        '39': 'b',
-        '40': 'c',
-
-        '41': 'c',
-        '42': 'd',
-        '43': 'b',
-        '44': 'c',
-        '45': 'c',
-        '46': 'd',
-        '47': 'c',
-        '48': 'b',
-        '49': 'd',
-        '50': 'd',
-
-        '51': 'd',
-        '52': 'e',
-        '53': 'd',
-        '54': 'c',
-        '55': 'd',
-        '56': 'b',
-        '57': 'b',
-        '58': 'c',
-        '59': 'c',
-        '60': 'd',
-
-        '61': 'c',
-        '62': 'b',
-        '63': 'c',
-        '64': 'b',
-        '65': 'a',
-        '66': 'c',
-        '67': 'd',
-        '68': 'a',
-        '69': 'c',
-        '70': 'c',
-
-        '71': 'b',
-        '72': 'b',
-        '73': 'd',
-        '74': 'b',
-        '75': 'c',
-        '76': 'a',
-        '77': 'c',
-        '78': 'c',
-        '79': 'd',
-        '80': 'c',
-    }
-
-    B = {
-        '1': 'c',
-        '2': 'd',
-        '3': 'd',
-        '4': 'c',
-        '5': 'c',
-        '6': 'c',
-        '7': 'd',
-        '8': 'a',
-        '9': 'b',
-        '10': 'c',
-
-        '11': 'c',
-        '12': 'c',
-        '13': 'c',
-        '14': 'c',
-        '15': 'd',
-        '16': 'b',
-        '17': 'b',
-        '18': 'c',
-        '19': 'b',
-        '20': 'e',
-
-        '21': 'b',
-        '22': 'b',
-        '23': 'c',
-        '24': 'b',
-        '25': 'a',
-        '26': 'c',
-        '27': 'b',
-        '28': 'c',
-        '29': 'c',
-        '30': 'a',
-
-        '31': 'e',
-        '32': 'a',
-        '33': 'c',
-        '34': 'c',
-        '35': 'c',
-        '36': 'c',
-        '37': 'b',
-        '38': 'c',
-        '39': 'e',
-        '40': 'b',
-
-        '41': 'b',
-        '42': 'b',
-        '43': 'd',
-        '44': 'c',
-        '45': 'b',
-        '46': 'b',
-        '47': 'a',
-        '48': 'e',
-        '49': 'b',
-        '50': 'a',
-
-        '51': 'b',
-        '52': 'd',
-        '53': 'c',
-        '54': 'd',
-        '55': 'c',
-        '56': 'c',
-        '57': 'c',
-        '58': 'c',
-        '59': 'd',
-        '60': 'e',
-
-        '61': 'c',
-        '62': 'b',
-        '63': 'd',
-        '64': 'b',
-        '65': 'd',
-        '66': 'c',
-        '67': 'd',
-        '68': 'c',
-        '69': 'b',
-        '70': 'e',
-
-        '71': 'd',
-        '72': 'b',
-        '73': 'c',
-        '74': 'd',
-        '75': 'c',
-        '76': 'c',
-        '77': 'c',
-        '78': 'd',
-        '79': 'd',
-        '80': 'c',
-    }
-
-
-class TestFourGroups:
-    group_1 = {1, 2, 3, 4, 22, 23, 24, 25, 43, 44, 45, 46, 64, 65, 66, 67, 85, 86, 87, 88, 89}
-    group_2 = {5, 6, 7, 8, 9, 26, 27, 28, 29, 47, 48, 49, 50, 68, 69, 70, 71, 90, 91, 92}
-    group_3 = {10, 11, 30, 31, 32, 51, 52, 53, 54, 72, 73, 74, 75, 93, 94, 95, 96}
-    group_4 = {12, 13, 14, 33, 34, 35, 55, 56, 76, 77, 97, 98}
-    group_5 = {15, 16, 36, 37, 57, 58, 78, 79, 80, 99, 100, 101}
-    group_6 = {17, 18, 38, 39, 59, 60, 81, 82, 102, 103}
-    group_7 = {19, 20, 21, 40, 41, 42, 61, 62, 63, 83, 84, 104, 105}
+    RANGE_1 = range(70, 101)
+    RANGE_2 = range(20, 70)
+    RANGE_3 = range(-39, 20)
+    RANGE_4 = range(-100, -39)
 
 
 def process_test_one(answers: list, is_man: bool):
+    points, unstables = get_points_for_test_one(answers)
+    percents = get_percent_for_test_one(points, is_man)
+    return get_result_for_test_one(percents, unstables)
+
+
+def get_points_for_test_one(answers: list) -> tuple:
     points_a = points_b = points_c = points_d = points_e = points_f = points_g = points_h = points_i = points_j = 0
+    unstable_e = unstable_b = False
 
     for i, answer in enumerate(answers):
         j = i + 1
+
+        if j == 22 and answer == 1:
+            unstable_e = True
+        if j == 197 and answer == 1:
+            unstable_b = True
 
         score_position = None
         if answer == 1:
@@ -1607,124 +1431,186 @@ def process_test_one(answers: list, is_man: bool):
         elif j in TestOne.feature_j:
             points_j += TestOne.scores[str(j)][score_position]
 
+    points = points_a, points_b, points_c, points_d, points_e, points_f, points_g, points_h, points_i, points_j
+    unstables = unstable_b, unstable_e
+    return points, unstables
+
+
+def get_percent_for_test_one(points, is_man):
+    points_a, points_b, points_c, points_d, points_e, points_f, points_g, points_h, points_i, points_j = points
+
     percent_class = TestOne.PercentForMan if is_man else TestOne.PercentForWoman
 
-
-
-def process_test_two(data: OrderedDict):
-    answers = data['answers']
-    is_man = data['is_man']
-    correct_answers = getattr(TestTwoAnswers, data['version'])
-    result = 0
-
-    for i, value in enumerate(answers):
-        j = i + 1
-        if value == correct_answers[str(j)]:
-            result += 1
-
-    if is_man:
-        result += 75
+    if points_a > percent_class.feature_a_max[0]:
+        percent_a = percent_class.feature_a_max[1]
+    elif points_a < percent_class.feature_a_min[0]:
+        percent_a = percent_class.feature_a_min[1]
     else:
-        result += 70
+        percent_a = percent_class.feature_a[points_a]
+
+    if points_b > percent_class.feature_b_max[0]:
+        percent_b = percent_class.feature_b_max[1]
+    elif points_b < percent_class.feature_b_min[0]:
+        percent_b = percent_class.feature_b_min[1]
+    else:
+        percent_b = percent_class.feature_b[str(points_b)]
+
+    if points_c > percent_class.feature_c_max[0]:
+        percent_c = percent_class.feature_c_max[1]
+    elif points_c < percent_class.feature_c_min[0]:
+        percent_c = percent_class.feature_c_min[1]
+    else:
+        percent_c = percent_class.feature_c[str(points_c)]
+
+    if points_d > percent_class.feature_d_max[0]:
+        percent_d = percent_class.feature_d_max[1]
+    elif points_d < percent_class.feature_d_min[0]:
+        percent_d = percent_class.feature_d_min[1]
+    else:
+        percent_d = percent_class.feature_d[str(points_d)]
+
+    if points_e > percent_class.feature_e_max[0]:
+        percent_e = percent_class.feature_e_max[1]
+    elif points_e < percent_class.feature_e_min[0]:
+        percent_e = percent_class.feature_e_min[1]
+    else:
+        percent_e = percent_class.feature_e[str(points_e)]
+
+    if points_f > percent_class.feature_f_max[0]:
+        percent_f = percent_class.feature_f_max[1]
+    elif points_f < percent_class.feature_f_min[0]:
+        percent_f = percent_class.feature_f_min[1]
+    else:
+        percent_f = percent_class.feature_f[str(points_f)]
+
+    if points_g > percent_class.feature_g_max[0]:
+        percent_g = percent_class.feature_g_max[1]
+    elif points_g < percent_class.feature_g_min[0]:
+        percent_g = percent_class.feature_g_min[1]
+    else:
+        percent_g = percent_class.feature_g[str(points_g)]
+
+    if points_h > percent_class.feature_h_max[0]:
+        percent_h = percent_class.feature_h_max[1]
+    elif points_h < percent_class.feature_h_min[0]:
+        percent_h = percent_class.feature_h_min[1]
+    else:
+        percent_h = percent_class.feature_h[str(points_h)]
+
+    if points_i > percent_class.feature_i_max[0]:
+        percent_i = percent_class.feature_i_max[1]
+    elif points_i < percent_class.feature_i_min[0]:
+        percent_i = percent_class.feature_i_min[1]
+    else:
+        percent_i = percent_class.feature_i[str(points_i)]
+
+    if points_j > percent_class.feature_j_max[0]:
+        percent_j = percent_class.feature_j_max[1]
+    elif points_j < percent_class.feature_j_min[0]:
+        percent_j = percent_class.feature_j_min[1]
+    else:
+        percent_j = percent_class.feature_j[str(points_j)]
+
+    return percent_a, percent_b, percent_c, percent_d, percent_e, percent_f, percent_g, percent_h, percent_i, percent_j
+
+
+def get_result_for_test_one(percents, unstables):
+    percent_a, percent_b, percent_c, percent_d, percent_e, percent_f, percent_g, percent_h, percent_i, percent_j = percents
+    unstable_b, unstable_e = unstables
+    result = {}
+
+    if percent_a in TestOne.RANGE_1:
+        result['a'] = 1
+    elif percent_a in TestOne.RANGE_2:
+        result['a'] = 2
+    elif percent_a in TestOne.RANGE_3:
+        result['a'] = 3
+    elif percent_a in TestOne.RANGE_4:
+        result['a'] = 4
+
+    if unstable_b:
+        result['b'] = 0
+    elif percent_b in TestOne.RANGE_1:
+        result['b'] = 1
+    elif percent_b in TestOne.RANGE_2:
+        result['b'] = 2
+    elif percent_b in TestOne.RANGE_3:
+        result['b'] = 3
+    elif percent_b in TestOne.RANGE_4:
+        result['b'] = 4
+
+    if percent_c in TestOne.RANGE_1:
+        result['c'] = 1
+    elif percent_c in TestOne.RANGE_2:
+        result['c'] = 2
+    elif percent_c in TestOne.RANGE_3:
+        result['c'] = 3
+    elif percent_c in TestOne.RANGE_4:
+        result['c'] = 4
+
+    if percent_d in TestOne.RANGE_1:
+        result['d'] = 1
+    elif percent_d in TestOne.RANGE_2:
+        result['d'] = 2
+    elif percent_d in TestOne.RANGE_3:
+        result['d'] = 3
+    elif percent_d in TestOne.RANGE_4:
+        result['d'] = 4
+
+    if unstable_e:
+        result['e'] = 0
+    elif percent_e in TestOne.RANGE_1:
+        result['e'] = 1
+    elif percent_e in TestOne.RANGE_2:
+        result['e'] = 2
+    elif percent_e in TestOne.RANGE_3:
+        result['e'] = 3
+    elif percent_e in TestOne.RANGE_4:
+        result['e'] = 4
+
+    if percent_f in TestOne.RANGE_1:
+        result['f'] = 1
+    elif percent_f in TestOne.RANGE_2:
+        result['f'] = 2
+    elif percent_f in TestOne.RANGE_3:
+        result['f'] = 3
+    elif percent_f in TestOne.RANGE_4:
+        result['f'] = 4
+
+    if percent_g in TestOne.RANGE_1:
+        result['g'] = 1
+    elif percent_g in TestOne.RANGE_2:
+        result['g'] = 2
+    elif percent_g in TestOne.RANGE_3:
+        result['g'] = 3
+    elif percent_g in TestOne.RANGE_4:
+        result['g'] = 4
+
+    if percent_h in TestOne.RANGE_1:
+        result['h'] = 1
+    elif percent_h in TestOne.RANGE_2:
+        result['h'] = 2
+    elif percent_h in TestOne.RANGE_3:
+        result['h'] = 3
+    elif percent_h in TestOne.RANGE_4:
+        result['h'] = 4
+
+    if percent_i in TestOne.RANGE_1:
+        result['i'] = 1
+    elif percent_i in TestOne.RANGE_2:
+        result['i'] = 2
+    elif percent_i in TestOne.RANGE_3:
+        result['i'] = 3
+    elif percent_i in TestOne.RANGE_4:
+        result['i'] = 4
+
+    if percent_j in TestOne.RANGE_1:
+        result['j'] = 1
+    elif percent_j in TestOne.RANGE_2:
+        result['j'] = 2
+    elif percent_j in TestOne.RANGE_3:
+        result['j'] = 3
+    elif percent_j in TestOne.RANGE_4:
+        result['j'] = 4
 
     return result
-
-
-def process_test_four(questions: list) -> str:
-    results = get_results_for_test_four(questions)
-    tests = get_tests_for_test_four(*results)
-    return result_for_test_four(*tests)
-
-
-def get_results_for_test_four(questions: list) -> tuple:
-    result_1 = result_2 = result_3 = result_4 = result_5 = result_6 = result_7 = 0
-
-    for i, value in enumerate(questions):
-        j = i + 1
-        if j in TestFourGroups.group_1:
-            if value:
-                result_1 += 1
-        elif j in TestFourGroups.group_2:
-            if value:
-                result_2 += 1
-        elif j in TestFourGroups.group_3:
-            if value:
-                result_3 += 1
-        elif j in TestFourGroups.group_4:
-            if value:
-                result_4 += 1
-        elif j in TestFourGroups.group_5:
-            if value:
-                result_5 += 1
-        elif j in TestFourGroups.group_6:
-            if value:
-                result_6 += 1
-        elif j in TestFourGroups.group_7:
-            if value:
-                result_7 += 1
-
-    return result_1, result_2, result_3, result_4, result_5, result_6, result_7
-
-
-def get_tests_for_test_four(result_1, result_2, result_3, result_4, result_5, result_6, result_7) -> tuple:
-    if result_1 > (len(TestFourGroups.group_1) - 1)/2:
-        test_1 = True
-    else:
-        test_1 = False
-
-    if result_2 > len(TestFourGroups.group_2)/2:
-        test_2 = True
-    else:
-        test_2 = False
-
-    if result_3 > len(TestFourGroups.group_3)/2:
-        test_3 = True
-    else:
-        test_3 = False
-
-    if result_4 > (len(TestFourGroups.group_4) - 1)/2:
-        test_4 = True
-    else:
-        test_4 = False
-
-    if result_5 > (len(TestFourGroups.group_5) - 1)/2:
-        test_5 = True
-    else:
-        test_5 = False
-
-    if result_6 > len(TestFourGroups.group_6)/2:
-        test_6 = True
-    else:
-        test_6 = False
-
-    if result_7 > (len(TestFourGroups.group_7) - 1)/2:
-        test_7 = True
-    else:
-        test_7 = False
-
-    return test_1, test_2, test_3, test_4, test_5, test_6, test_7
-
-
-def result_for_test_four(test_1, test_2, test_3, test_4, test_5, test_6, test_7) -> str:
-    if test_1:
-        if test_2:
-            if test_4:
-                return '3.6-4.0'
-            else:
-                return '3.1-3.5'
-        else:
-            if test_5:
-                return '2.6-3.0'
-            else:
-                return '2.1-2.5'
-    else:
-        if test_3:
-            if test_6:
-                return '1.5-2.0'
-            else:
-                return '0.1-0.5'
-        else:
-            if test_7:
-                return '0.6-1.0'
-            else:
-                return '0.1-0.5'
--- tests/views.py
+++ tests/views.py
@@ -1,10 +1,14 @@
 from drf_yasg.utils import swagger_auto_schema
+from rest_framework import status
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.permissions import AllowAny
 
 from tests.serializers import TestFourSerializer, TestTwoSerializer, TestOneSerializer
-from tests.services import process_test_four, process_test_two, process_test_one
+from tests.services.test_one import process_test_one
+from tests.services.test_two import process_test_two
+from tests.services.test_four import process_test_four
+from utils.tools import log_exception
 
 
 class TestOneView(APIView):
@@ -12,10 +16,14 @@ class TestOneView(APIView):
 
     @swagger_auto_schema(request_body=TestOneSerializer)
     def post(self, request):
-        serializer = TestOneSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        result = process_test_one(**serializer.validated_data)
-        return Response({'result': result})
+        try:
+            serializer = TestOneSerializer(data=request.data)
+            serializer.is_valid(raise_exception=True)
+            result = process_test_one(**serializer.validated_data)
+            return Response({'result': result})
+        except Exception as e:
+            log_exception(e, 'Error in TestOneView.post()')
+            return Response({'message': str(e)}, status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
 class TestTwoView(APIView):
