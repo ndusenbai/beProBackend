27b5eec5574c2212204e781699935dd1692d0ac2
Bakhtiyar Ospanov <b.p.ospanov@gmail.com>
2022-09-02 17:40:52 +0600

    fix: celery absence_checks logs

--- timesheet/tasks.py
+++ timesheet/tasks.py
@@ -3,6 +3,7 @@ from datetime import date, datetime
 
 from companies.models import Role
 from timesheet.models import TimeSheetChoices, TimeSheet
+from utils.tools import log_message, log_exception
 
 
 def create_absent_timesheet(role, now_date, now_time):
@@ -15,22 +16,30 @@ def create_absent_timesheet(role, now_date, now_time):
                              comment='Automatically filled by auto check')
 
 
+def absence_check_request(request):
+    absence_check()
+
+
 @shared_task()
 def absence_check():
     now_date = date.today()
     now_time = datetime.now().time()
+    log_message(f'started absence_check at {now_date} {now_time}')
 
     roles = Role.objects.prefetch_related('timesheet', 'employee_schedules', 'department').all()
 
     for role in roles:
-        if not role.timesheet.filter(day__year=now_date.year,
-                                     day__month=now_date.month,
-                                     day__day=now_date.day):
+        try:
+            if not role.timesheet.filter(day__year=now_date.year,
+                                         day__month=now_date.month,
+                                         day__day=now_date.day):
 
-            today_in_employee_schedule = role.employee_schedules.filter(week_day=now_date.weekday())
-            if today_in_employee_schedule:
-                create_absent_timesheet(role, now_date, now_time)
-            else:
-                today_in_departament_schedule = role.department.department_schedules.filter(week_day=now_date.weekday())
-                if today_in_departament_schedule:
+                today_in_employee_schedule = role.employee_schedules.filter(week_day=now_date.weekday())
+                if today_in_employee_schedule:
                     create_absent_timesheet(role, now_date, now_time)
+                else:
+                    today_in_departament_schedule = role.department.department_schedules.filter(week_day=now_date.weekday())
+                    if today_in_departament_schedule:
+                        create_absent_timesheet(role, now_date, now_time)
+        except Exception as e:
+            log_exception(e, 'error in celery beat absence_check')
