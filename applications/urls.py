775a4c3088765ac33a9039db835e48cc97c71af8
Bakhtiyar Ospanov <b.p.ospanov@gmail.com>
2022-09-06 14:41:16 +0600

    fix: create company application fix

--- applications/urls.py
+++ applications/urls.py
@@ -6,7 +6,7 @@ router = DefaultRouter()
 
 router.register('test-application', views.TestApplicationView, basename='test-application-view')
 router.register('tariff-application', views.TariffApplicationView, basename='tariff-application-view')
-router.register('app-to-create-company', views.ApplicationToCreateCompanyViewSet, basename='app-to-create-company')
 router.register('app-to-create-company/create', views.ApplicationToCreateCompanyCreateViewSet, basename='app-to-create-company-create')
+router.register('app-to-create-company', views.ApplicationToCreateCompanyViewSet, basename='app-to-create-company')
 
 urlpatterns = [] + router.urls
