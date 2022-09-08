2b642bbaecb3ae6d79356a7bedd6c2f7ee54db71
Bakhtiyar Ospanov <b.p.ospanov@gmail.com>
2022-09-08 18:02:08 +0600

    fix: timesheet logs

--- timesheet/views.py
+++ timesheet/views.py
@@ -16,8 +16,7 @@ from timesheet.services import create_check_in_timesheet, get_last_timesheet_act
 from timesheet.utils import EmployeeTooFarFromDepartment, FillUserStatistic
 from utils.manual_parameters import QUERY_YEAR, QUERY_MONTH, QUERY_ROLE
 from utils.permissions import TimeSheetPermissions, ChangeTimeSheetPermissions, CheckPermission
-from utils.tools import log_exception
-
+from utils.tools import log_exception, log_message
 
 User = get_user_model()
 
@@ -33,9 +32,16 @@ class TimeSheetViewSet(ListModelMixin, UpdateModelMixin, GenericViewSet):
         """
         Получить расписание за определенный месяц на роль
         """
+        # TODO: delete this after testing
+        if request.user.email == 'makenoverkebulan@gmail.com':
+            log_message('makenoverkebulan@gmail.com uses timesheet: start')
         serializer = TimeSheetListSerializer(data=self.request.query_params)
         serializer.is_valid(raise_exception=True)
         data = get_timesheet_by_month(**serializer.validated_data)
+        if request.user.email == 'makenoverkebulan@gmail.com':
+            log_message('makenoverkebulan@gmail.com uses timesheet: end')
+            import json
+            log_message(json.dumps(data, indent=4, sort_keys=True, default=str))
         return Response(data=data)
 
     @swagger_auto_schema(request_body=TimeSheetUpdateSerializer)
