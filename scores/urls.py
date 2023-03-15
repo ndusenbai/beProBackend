2084ab4aa8dec872f50ce26132531ca9efc1dfae
Arslan <4rs1ck@gmail.com>
2023-03-15 15:38:19 +0600

    test

--- scores/urls.py
+++ scores/urls.py
@@ -4,7 +4,7 @@ from scores import views
 router = DefaultRouter()
 
 router.register('reason', views.ReasonViewSet, basename='reason-viewset')
-router.register('score/feed', views.ScoreFeedListView, basename='score-feed-view')
+# router.register('score/feed', views.ScoreFeedListView, basename='score-feed-view')
 router.register('score', views.ScoreViewSet, basename='score-viewset')
 router.register('month-scores', views.MonthScoresViewSet, basename='month-scores')
 
