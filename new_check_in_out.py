c141d743b506b0775d57f0f412bd879b30b001c8
Arslan <4rs1ck@gmail.com>
2023-03-31 17:12:32 +0600

    fixed script

--- new_check_in_out.py
+++ new_check_in_out.py
@@ -7,9 +7,10 @@ from datetime import datetime
 from django.apps import apps
 
 
-def copy_time_fields(apps):
-    TimeSheet = apps.get_model('timesheet', 'TimeSheet')
-    for timesheet in TimeSheet.objects.all():
-        timesheet.check_in = datetime.combine(timesheet.day, timesheet.check_in)
-        timesheet.check_out = datetime.combine(timesheet.day, timesheet.check_out)
-        timesheet.save()
+TimeSheet = apps.get_model('timesheet', 'TimeSheet')
+for timesheet in TimeSheet.objects.all():
+    timesheet.check_in = datetime.combine(timesheet.day, timesheet.check_in)
+    timesheet.check_out = datetime.combine(timesheet.day, timesheet.check_out)
+    timesheet.save()
+
+
