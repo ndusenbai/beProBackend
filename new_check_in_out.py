568325e02805f87b0d5ca87101787497ba11ef1d
Arslan <4rs1ck@gmail.com>
2023-03-31 17:18:55 +0600

    added timezone support to new check_in check_out

--- new_check_in_out.py
+++ new_check_in_out.py
@@ -5,14 +5,15 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 django.setup()
 from datetime import datetime
 from django.apps import apps
+import pytz
 
 
 TimeSheet = apps.get_model('timesheet', 'TimeSheet')
 for timesheet in TimeSheet.objects.all():
     if timesheet.check_in:
-        timesheet.check_in_new = datetime.combine(timesheet.day, timesheet.check_in)
+        tz = pytz.timezone(timesheet.timezone)
+        timesheet.check_in_new = tz.localize(datetime.combine(timesheet.day, timesheet.check_in))
     if timesheet.check_out:
-        timesheet.check_out_new = datetime.combine(timesheet.day, timesheet.check_out)
+        tz = pytz.timezone(timesheet.timezone)
+        timesheet.check_out_new = tz.localize(datetime.combine(timesheet.day, timesheet.check_out))
     timesheet.save()
-
-
