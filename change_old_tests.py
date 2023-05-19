1a2ead961d0b8e408579a6949b8b5fe60251af32
Arslan <4rs1ck@gmail.com>
2023-05-19 16:55:54 +0600

    feat(bp-278): added 3 functions to change test 2,3,4

--- change_old_tests.py
+++ change_old_tests.py
@@ -6,29 +6,122 @@ def update_old_result_for_test_one():
     for test_one in test_ones:
 
         result = test_one.result
-        conclusions = result['conclusions']
-        new_conclusions = []
-        for conclusion in conclusions:
-                new_conclusions.append(
-                    {
-                        'ru': conclusion,
-                        'kz': conclusion
-                    }
-                )
-        result['conclusions'] = new_conclusions
+        try:
+            conclusions = result['conclusions']
+            new_conclusions = []
+            for conclusion in conclusions:
+                    new_conclusions.append(
+                        {
+                            'ru': conclusion,
+                            'kz': conclusion
+                        }
+                    )
+            result['conclusions'] = new_conclusions
 
-        characteristics = result['characteristics']
+            characteristics = result['characteristics']
+            new_characteristics = []
+            for characteristic in characteristics:
+                for category in characteristic:
+
+                    new_characteristics.append(
+                        {
+                            f'{category}':{
+                                            'ru': characteristic[category],
+                                            'kz': characteristic[category]
+                            }
+                        }
+                    )
+
+            result['characteristics'] = new_characteristics
+            test_one.result = result
+            print('+=================================================================')
+            print('NEW_RESULT_OF_TEST_ONE_WITH_ID', test_one.id)
+            print(result)
+            print("END_OF_RESULT_OF_TEST_ONE_WITH_ID", test_one.id)
+            print('+=================================================================')
+        except KeyError:
+            pass
+
+
+def update_old_result_for_test_two():
+    test_twos = Test.objects.filter(test_type=2, status=2)
+    for test_two in test_twos:
+        result = test_two.result
+        try:
+            new_result = {
+                'kz': {
+                    'classification': result['classification'],
+                    'percent': result['percent'],
+                    'summary': result['summary']
+                },
+                'points': result['points'],
+                'ru': {
+                    'classification': result['classification'],
+                    'percent': result['percent'],
+                    'summary': result['summary']
+                }
+            }
+            test_two.result = new_result
+            print('+=================================================================')
+            print('NEW_RESULT_OF_TEST_TWO_WITH_ID', test_two.id)
+            print(test_two.result)
+            print("END_OF_RESULT_OF_TEST_TWO_WITH_ID", test_two.id)
+            print('+=================================================================')
+        except KeyError:
+            pass
+
+
+def update_old_result_for_test_three():
+    test_threes = Test.objects.filter(test_type=3, status=2)
+    for test_three in test_threes:
+        result = test_three.result
+        try:
+            new_result = {
+                'kz': {
+                    'description': result['description']
+                },
+                'points': result['points'],
+                'ru': {
+                    'description': result['description']
+                }
+            }
+            test_threes.result = new_result
+            print('+=================================================================')
+            print('NEW_RESULT_OF_TEST_THREE_WITH_ID', test_three.id)
+            print(test_threes.result)
+            print("END_OF_RESULT_OF_TEST_THREE_WITH_ID", test_three.id)
+            print('+=================================================================')
+        except KeyError:
+            pass
+
+
+def update_old_result_for_test_four():
+    test_fours = Test.objects.filter(test_type=4, status=2)
+    for test_four in test_fours:
+        result = test_four.result
         new_characteristics = []
-        for characteristic in characteristics:
-            for category in characteristic:
-
+        try:
+            for characteristic in result['characteristics']:
                 new_characteristics.append(
                     {
-                        f'{category}':{
-                                        'ru': characteristic[category],
-                                        'kz': characteristic[category]
+                        'kz': {
+                            'points': characteristic['points'],
+                            'description': characteristic['description'],
+                            'characteristic': characteristic['characteristic']
+                        },
+                        'ru': {
+                            'points': characteristic['points'],
+                            'description': characteristic['description'],
+                            'characteristic': characteristic['characteristic']
                         }
                     }
                 )
-
-        result['characteristics'] = new_characteristics
+            result['characteristics'] = new_characteristics
+            test_four.result = result
+            print('+=================================================================')
+            print('NEW_RESULT_OF_TEST_FOUR_WITH_ID', test_four.id)
+            print(test_four.result)
+            print("END_OF_RESULT_OF_TEST_FOUR_WITH_ID", test_four.id)
+            print('+=================================================================')
+        except KeyError:
+            pass
