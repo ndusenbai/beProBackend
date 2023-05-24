0dcfe81fd44027df35306095f42487dbf1233305
Arslan <4rs1ck@gmail.com>
2023-05-24 14:40:06 +0600

    fix(BP-295):fix url error

--- notifications/urls.py
+++ notifications/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 
-from notifications.views import NotificationHandler, TestNotificationAPI
+from notifications.views import NotificationHandler
 
 urlpatterns = [
     path('check/', NotificationHandler.as_view(), name='notification-cron'),
-    path('test-notifications/', TestNotificationAPI.as_view(), name='test-notifications')
 ]
 
