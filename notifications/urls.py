58c523371c7a832a696a476001b3c0f23ba51840
Arslan <4rs1ck@gmail.com>
2023-05-24 14:33:32 +0600

    fix(BP-295):fix view urls

--- notifications/urls.py
+++ notifications/urls.py
@@ -4,6 +4,6 @@ from notifications.views import NotificationHandler, TestNotificationAPI
 
 urlpatterns = [
     path('check/', NotificationHandler.as_view(), name='notification-cron'),
-    path('test-notifications/', TestNotificationAPI.as_view())
+    path('test-notifications/', TestNotificationAPI.as_view(), name='test-notifications')
 ]
 
