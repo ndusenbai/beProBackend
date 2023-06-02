ac1c184dfdbcc773471ede08fa018d7da9917883
Arslan <4rs1ck@gmail.com>
2023-06-02 19:36:41 +0600

    fix

--- notifications/cron.py
+++ notifications/cron.py
@@ -1,14 +1,5 @@
 import requests
-import environ
-env = environ.Env()
-environ.Env.read_env()
 
-prod = bool(env('PROD'))
-
-url = 'http://89.46.34.208/api/notification/check/'
-
-
-if prod:
-    url = 'https://bepro.kz/api/notification/check/'
+url = 'https://bepro.kz/api/notification/check/'
 
 resp = requests.get(url)
