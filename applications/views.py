b061952e60c34420846170567ca504e2b36bede1
Arslan <4rs1ck@gmail.com>
2023-06-12 16:29:18 +0600

    added DjangoFilter to tariff application

--- applications/views.py
+++ applications/views.py
@@ -85,7 +85,7 @@ class ApplicationToCreateCompanyCreateViewSet(CreateModelMixin, GenericViewSet):
 class TariffApplicationView(ListModelMixin, RetrieveModelMixin, UpdateModelMixin, GenericViewSet):
     permission_classes = (TariffApplicationPermission,)
     queryset = TariffApplication.objects.order_by('-created_at')
-    filter_backends = (SearchFilter,)
+    filter_backends = (SearchFilter, DjangoFilterBackend)
     filterset_fields = ('owner',)
     search_fields = ('owner__first_name', 'owner__last_name', 'owner__middle_name')
     http_method_names = ['get', 'put']
